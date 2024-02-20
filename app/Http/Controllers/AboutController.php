49b4055e4473baa90d93d420dd8f2fd58f08f694
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-02-20 12:29:56 +0700

    langakah-langkah praktikum Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama: Lukman Eka Septiawan | NIM: 2241720049';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,38 +22,58 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return "Hello World";
-});
+// Route::get('/hello', function () {
+//     return "Hello World";
+// });
 
-Route::get('/world', function () {
-    return "World";
-});
+// Route::get('/hello', function () {
+//     return "Hello World";
+// });
 
-Route::get('/message', function () {
-    return "Selamat Datang";
-});
+// Route::get('/world', function () {
+//     return "World";
+// });
 
-Route::get('/about', function () {
-    return "Nama\t: Lukman Eka Septiawan NIM\t: 2241720049";
-});
+// Route::get('/message', function () {
+//     return "Selamat Datang";
+// });
+
+// Route::get('/about', function () {
+//     return "Nama\t: Lukman Eka Septiawan NIM\t: 2241720049";
+// });
 
 // Route::get('/user/{name}', function ($name) {
 //     return "Nama saya $name";
 // });
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return "Post ke-" . $postId . " Komentar ke-" . $commentId;
-});
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return "Post ke-" . $postId . " Komentar ke-" . $commentId;
+// });
 
-Route::get('/articles/{id}', function ($articleId) {
-    return "Halaman Artikel dengan ID $articleId";
-});
+// Route::get('/articles/{id}', function ($articleId) {
+//     return "Halaman Artikel dengan ID $articleId";
+// });
 
 // Route::get('/user/{name?}', function ($name='John') {
 //     return "Nama saya $name";
 // });
 
-Route::get('/user/profile', function () {
-    //
-}) -> name('profile');
+// Route::get('/user/profile', function () {
+//     //
+// }) -> name('profile');
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::resource('/photos', PhotoController::class) -> only ([
+    'index', 'show'
+]);
+
+Route::resource('/photos', PhotoController::class) -> except ([
+    'create', 'store', 'update', 'destroy'
+]);
