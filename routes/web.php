5af53a9b9b14412d7975a6babb1a0e36acc52e14
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-02-20 11:09:11 +0700

    langkah-langkah praktikum route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/message', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "Nama\t: Lukman Eka Septiawan NIM\t: 2241720049";
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return "Nama saya $name";
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return "Post ke-" . $postId . " Komentar ke-" . $commentId;
+});
+
+Route::get('/articles/{id}', function ($articleId) {
+    return "Halaman Artikel dengan ID $articleId";
+});
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return "Nama saya $name";
+// });
+
+Route::get('/user/profile', function () {
+    //
+}) -> name('profile');
